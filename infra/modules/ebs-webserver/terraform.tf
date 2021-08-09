374a40e1cd94b8b1dbba9300b829fb9d85694863
Saurav <sauravads123@gmail.com>
2021-08-09 11:46:56 +0545

    minor

--- infra/modules/ebs-webserver/terraform.tf
+++ infra/modules/ebs-webserver/terraform.tf
@@ -1,11 +1,14 @@
 variable "beanstalk_application_name" {
   type=string
+  description = "Beanstalk application name"
 }
 
 variable "beanstalk_environment_name" {
   type=string
+  description = "Beanstalk environment name"
 }
 
 variable "beanstalk_environment_solution_stack" {
   type=string
+  description = "Beanstalk solution stack name"
 }
